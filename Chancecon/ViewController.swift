ec61378c811cfcd3681e9f7194ccedd60d2d607d
Freedom <fang.ming@zoho.com>
2016-06-17 12:09:43 +1200

    Initial Commit

---
+++ Chancecon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chancecon
+//
+//  Created by Freedom on 17/06/16.
+//  Copyright © 2016 Chancellor Construction Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
